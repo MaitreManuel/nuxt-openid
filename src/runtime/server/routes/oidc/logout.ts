9d3596d5837e06652a0e4b9e4b288fd152a63eed
Trystan EVENO <trystan.eveno@kleegroup.com>
2025-06-04 16:24:16 +0200

    [main] ensure rightfull protocol for logout && trim useless callback worker

--- src/runtime/server/routes/oidc/logout.ts
+++ src/runtime/server/routes/oidc/logout.ts
@@ -1,4 +1,4 @@
-import { deleteCookie, defineEventHandler, getCookie } from 'h3';
+import { deleteCookie, defineEventHandler, getCookie, getRequestProtocol, getRequestHost } from 'h3';
 import { getCallbackUrl, getDefaultBackUrl, getRedirectUrl } from '../../../utils/utils';
 import { initClient } from '../../../utils/issueclient';
 import { useRuntimeConfig } from '#imports';
@@ -12,17 +12,19 @@ export default defineEventHandler(async (event) => {
     console.log('[LOGOUT]: oidc/logout calling');
   }
 
-  const redirectUrl = getRedirectUrl(req.url, op.redirectLogoutUrl);
-  const callbackUrl = getCallbackUrl(op.callbackLogoutUrl, redirectUrl, req.headers);
-  const defCallBackUrl = getDefaultBackUrl(redirectUrl, req.headers);
+  // const redirectUrl = getRedirectUrl(req.url, op.redirectLogoutUrl);
+  // const callbackUrl = getCallbackUrl(op.callbackLogoutUrl, redirectUrl, req.headers);
+  // const defCallBackUrl = getDefaultBackUrl(redirectUrl, req.headers);
+  const redirectURI = `${getRequestProtocol(event)}://${getRequestHost(event)}/`;
 
   const refreshToken = getCookie(event, config.cookiePrefix + 'refresh_token');
 
   if (config.debug) {
     console.log(`[LOGOUT]: req.url: ${req.url}`);
-    console.log(`[LOGOUT]: op.redirectLogoutUrl: ${op.redirectLogoutUrl}, redirectUrl: ${redirectUrl}`);
-    console.log(`[LOGOUT]: op.callbackLogoutUrl: ${op.callbackLogoutUrl}, callbackUrl: ${callbackUrl}`);
-    console.log(`[LOGOUT]: defCallBackUrl: ${defCallBackUrl}`);
+    // console.log(`[LOGOUT]: op.redirectLogoutUrl: ${op.redirectLogoutUrl}, redirectUrl: ${redirectUrl}`);
+    // console.log(`[LOGOUT]: op.callbackLogoutUrl: ${op.callbackLogoutUrl}, callbackUrl: ${callbackUrl}`);
+    // console.log(`[LOGOUT]: defCallBackUrl: ${defCallBackUrl}`);
+    console.log(`[LOGOUT]: redirectURI: ${redirectURI}`);
     console.log(`[LOGOUT]: protocol: ${req.headers['x-forwarded-proto']}, host: ${req.headers.host}`);
   }
 
@@ -31,12 +33,12 @@ export default defineEventHandler(async (event) => {
   deleteCookie(event, config.cookiePrefix + 'refresh_token');
   deleteCookie(event, config.cookiePrefix + 'user_info');
 
-  const issueClient = await initClient(op, req, [callbackUrl, defCallBackUrl]);
+  const issueClient = await initClient(op, req, [redirectURI]);
   const tokenSet = await issueClient.refresh(refreshToken);
 
   const parameters = {
     id_token_hint: tokenSet.id_token,
-    post_logout_redirect_uri: callbackUrl,
+    post_logout_redirect_uri: redirectURI,
   };
   const logoutUrl = issueClient.endSessionUrl(parameters);
 
